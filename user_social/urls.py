52e6c02ab922137ef12939d9ac950fbb7cc76ba1
devvlopar <devangsingh34@gmail.com>
2022-04-08 19:51:31 +0530

    login_register

--- user_social/urls.py
+++ user_social/urls.py
@@ -3,8 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.register, name='register'),
-    path('otp', views.otp, name='otp'),
+    path('register/', views.register, name='register'),
+    path('otp/', views.otp_fun, name='otp'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('forgot/', views.forgot, name='forgot'),
+    path('profile/', views.profile, name='profile'),
+
+    
+
+    path('notification/', views.notification, name='notification'),
+
+
+
+
 
 
 ]
--- user_social/views.py
+++ user_social/views.py
@@ -1,12 +1,20 @@
-import email
-import re
-from django.shortcuts import render
+from random import randrange
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 from user_social.models import User
+from django.conf import settings
 
-# Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    try:
+        uid = User.objects.get(email=request.session['email'])
+        return render(request,'index.html',{'uid':uid})
+    except:
+        return render(request,'login.html')
+
+
 
 def register(request):
     if request.method == 'POST':
@@ -15,15 +23,80 @@ def register(request):
             msg = 'Email Already Registered.'
             return render(request,'register_user.html',{'message':msg})
         except:
-            
-
-
+            if request.POST['password'] == request.POST['rpassword']:    
+                global otp, user_data
+                user_data = {
+                    'fullname': request.POST['fullname'],
+                    'email': request.POST['email'],
+                    'password': request.POST['password'],
+                }
+                otp = randrange(100000,999999)
+                subject = 'Email Verification Social_Media_App.'
+                message = f'Your OTP is {otp}.'
+                msg = 'Check Your MailBox.'
+                email_from = settings.EMAIL_HOST_USER
+                recipient_list = [request.POST['email'], ]
+                send_mail( subject, message, email_from, recipient_list )
+                return render(request,'otp.html',{'message':msg})
+            return render(request, 'register_user.html', {'message':'Both passwords are not same.'})
     return render(request, 'register_user.html')
 
-def otp(request):
-    if request.method == 'POST':
-        if request.POST['otp'] == request.POST['user_otp']:
-            pass
 
-            
+
+def otp_fun(request):
+    if request.method == 'POST':
+        global otp, user_data
+        if request.POST['user_otp'] == str(otp):
+            User.objects.create(
+                fullname = user_data['fullname'],
+                email = user_data['email'],
+                password = user_data['password']
+            )
+            msg = 'Account is Created Successfully.'
+            return render(request,'login.html',{'message':msg})     
+        else:
+            return render(request, 'otp.html',{'message':'otp comparision error'})   
     return render(request, 'otp.html')
+
+
+
+def login(request):
+    try:
+        uid = User.objects.get(email=request.session['email'])
+        return render(request,'index.html',{'uid':uid})
+    except:
+        if request.method == 'POST':
+            try:
+                uid = User.objects.get(email=request.POST['email'])
+                if request.POST['password'] == uid.password:
+                    request.session['email'] = request.POST['email']
+                    return redirect('index')
+                return render(request,'login.html',{'message':'Inncorrect password!!'})
+            except:
+                message = 'Email is not registered.'
+                return render(request,'login.html',{'message':message})
+    return render(request,'login.html')
+
+
+
+def logout(request):
+    del request.session['email']
+    return render(request,'login.html')
+
+
+
+def forgot(request):
+    return render(request, 'login.html')
+
+
+
+
+def notification(request):
+    return render(request,'login.html')
+
+
+
+def profile(request):
+    return render(request,'profile.html')
+
+
